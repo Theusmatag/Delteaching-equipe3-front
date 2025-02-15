0943723b33a9c02d433a7898f2bca7e072eac86e
matheus Tavares <matheus.tavares@delbank.com.br>
2025-02-15 16:02:27 -0300

    feat: adjustment in main method base

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,8 @@
+import 'package:dealteachingfront/delteaching_base.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(const DelteachingBase());
 }
